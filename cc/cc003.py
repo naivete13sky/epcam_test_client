cb9290357ec5b28b1f14e33c842ab22cdbbe83d4
naivete13sky <chen320821@163.com>
2022-09-30 17:03:21 +0800

    20220930 18 准备增加输出gerber测试

--- cc/cc003.py
+++ cc/cc003.py
@@ -2,3 +2,10 @@ import sys,os
 sys.path.append(r'C:\cc\python\epwork\dms\job_manage\epcam')
 import epcam
 epcam.init()
+
+
+
+
+
+
+
---
+++ cc/epcam_out.py
@@ -0,0 +1,7 @@
+
+
+
+if __name__ =='__main__':
+    pass
+    tgz_path = r'C:\AutoComparison\job'
+    # output(tgz_path, output_path, config_path)
