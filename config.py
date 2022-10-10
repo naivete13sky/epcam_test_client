201fa385140996810053c2024699e5bc498f1080
naivete13sky <chen320821@163.com>
2022-10-10 19:36:02 +0800

    20221010 3 cam版本换为055s13

--- config.py
+++ config.py
@@ -17,7 +17,8 @@ class RunConfig:
     # ep_cam_path = r'C:\cc\ep_local\product\EP-CAM\version\20220921\EP-CAM_beta_2.28.054_s39_jiami\Release'
     # ep_cam_path = r'C:\cc\ep_local\product\EP-CAM\version\20220922\EP-CAM_beta_2.28.054_s43_jiami\Release'
     # ep_cam_path = r'C:\cc\ep_local\product\EP-CAM\version\20220928\EP-CAM_beta_2.29.055_s1_u3_jiami\Release'
-    ep_cam_path = r'C:\cc\ep_local\product\EP-CAM\version\20220930\EP-CAM_beta_2.29.055_s6_jiami\Release'
+    # ep_cam_path = r'C:\cc\ep_local\product\EP-CAM\version\20220930\EP-CAM_beta_2.29.055_s6_jiami\Release'
+    ep_cam_path = r'C:\cc\ep_local\product\EP-CAM\version\20221010\EP-CAM_beta_2.29.055_s13_jiami\Release'
 
     epcam_python_path=r'C:\EPSemicon\cc\epcam'
 
