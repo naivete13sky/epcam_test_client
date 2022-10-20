e66fccba149800d672f5a0d7a91a420ee2c9cdbf
naivete13sky <chen320821@163.com>
2022-10-20 10:09:13 +0800

    20221020 2 修改G方法中

--- g_cc_method_no_django-old1.py
+++ g_cc_method_no_django-old1.py
@@ -14,7 +14,9 @@ LAYER_COMPARE_JSON = 'layer_compare.json'
 
 class Asw():
     def __init__(self,gateway_path):
+        # gateway_path=os.path.join(os.path.abspath(os.path.join(os.getcwd(), "..")),"method\g\gateway.exe")
         self.gateway_path=gateway_path
+        # print(gateway_path)
         command = '{} 1'.format(self.gateway_path)
         self.process = subprocess.Popen(command, stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
 
@@ -25,26 +27,187 @@ class Asw():
         self.process.stdin.write((cmd + '\n').encode())
         self.process.stdin.flush()
         line = self.process.stdout.readline()
+        # print(line)
+        print(line.decode('utf-8'))
         ret = int(line.decode().strip())
         return ret
 
+    def layer_compare_g1(self, paras, _type):
+        print("*" * 100)
+        result=""
+        try:
+            jobpath1 = paras['jobpath1']
+            job_name_1=jobpath1.split("\\")[-1]
+            step1 = paras['step1']
+            #layer1 = paras['layer1']
+            jobpath2 = paras['jobpath2']
+            step2 = paras['step2']
+            #layer2 = paras['layer2']
+            #layer2_ext = paras['layer2_ext']
+            tol = paras['tol']
+            #map_layer = paras['map_layer']
+            #map_layer_res = paras['map_layer_res']
+        except Exception as e:
+            print(e)
+            print("*"*100)
+            return result
 
+        if not os.path.exists(jobpath1):
+            print('{} does not exist'.format(jobpath1))
+            return result
+        if not os.path.exists(jobpath2):
+            print('{} does not exist'.format(jobpath2))
+            return result
+
+        job1 = os.path.basename(jobpath1)
+        job2 = os.path.basename(jobpath2)
+        layer_cp = ''
+        # print('COM compare_layers,layer1={},job2={},step2={},layer2={},layer2_ext={},tol={},area=global,consider_sr=yes,ignore_attr=,map_layer={},map_layer_res={}'.format(
+        #         layer1, job2, step2, layer2, layer2_ext, tol, map_layer, map_layer_res))
+        cmd_list1 = []
+        if _type == 0:
+            cmd_list1 = [
+                'COM import_job,db=genesis,path={},name={},analyze_surfaces=no'.format(jobpath1, job1),
+                'COM import_job,db=genesis,path={},name={},analyze_surfaces=no'.format(jobpath2, job2),
+                'COM check_inout,mode=out,type=job,job={}'.format(job1),
+                'COM clipb_open_job,job={},update_clipboard=view_job'.format(job1),
+                'COM open_job,job={}'.format(job1),
+                'COM open_entity,job={},type=step,name={},iconic=no'.format(job1, step1),
+                'COM units,type=inch',
+                'COM open_job,job={}'.format(job2)
+            ]
+            cmd_list2 = [
+                # 'COM editor_page_close',
+                # 'COM check_inout,mode=in,type=job,job={}'.format(job1),
+                # 'COM close_job,job={}'.format(job1),
+                # 'COM close_form,job={}'.format(job1),
+                # 'COM close_flow,job={}'.format(job1),
+                # 'COM delete_entity,job=,type=job,name={}'.format(job1),
+                # 'COM close_form,job={}'.format(job1),
+                # 'COM close_flow,job={}'.format(job1),
+                # # 'COM close_job,job={}'.format(job2),
+                # # 'COM close_form,job={}'.format(job2),
+                # # 'COM close_flow,job={}'.format(job2),
+                # 'COM delete_entity,job=,type=job,name={}'.format(job2),
+                # 'COM close_form,job={}'.format(job2),
+                # 'COM close_flow,job={}'.format(job2)
+            ]
+        elif _type == 1:
+            layer1 = paras['layer1']
+            layer2 = paras['layer2']
+            map_layer = paras['map_layer']
+            layer2_ext = paras["layer2_ext"]
+            map_layer_res = paras['map_layer_res']
+            layer_cp = layer2 + layer2_ext
+            cmd_list1 = [
+                'COM compare_layers,layer1={},job2={},step2={},layer2={},layer2_ext={},tol={},area=global,consider_sr=yes,ignore_attr=,map_layer={},map_layer_res={}'.format(
+                    layer1, job2, step2, layer2, layer2_ext, tol, map_layer, map_layer_res)
+            ]
+            cmd_list2 = [
+            ]
+        else:
+            cmd_list1 = [
+                'COM save_job,job={},override=no'.format(job1),
+                'COM editor_page_close',
+                'COM check_inout,mode=out,type=job,job={}'.format(job1),
+                'COM close_job,job={}'.format(job1),
+                'COM close_form,job={}'.format(job1),
+                'COM close_flow,job={}'.format(job1),
+                'COM close_job,job={}'.format(job2),
+                'COM close_form,job={}'.format(job2),
+                'COM close_flow,job={}'.format(job2)
+            ]
+            cmd_list2 = [
+            ]
+
+        for cmd in cmd_list1:
+            print(cmd)
+            ret = self.exec_cmd(cmd)
+            if ret != 0:
+                print('inner error')
+                return result
+
+        time.sleep(1)
+        # features=(r"C:\genesis\fw\jobs\{}\steps\{}\layers\diff\features".format(job_name_1,step1))
+        # features_Z = (r"C:\genesis\fw\jobs\{}\steps\{}\layers\diff\features.Z".format(job_name_1, step1))
+        # print(features,"\n",features_Z)
+        # if os.path.isfile(features_Z):
+        #     pass
+        #     compress=Compress()
+        #     compress.uncompress_z(features_Z)
+
+
+        # try:
+        #     f=open(features,"r")
+        # except Exception as e:
+        #     print("未能比对！！！请重新执行比对！！！")
+        #     result="未比对"
+        #     for cmd in cmd_list2:
+        #         self.exec_cmd(cmd)
+        #     return result
+
+
+        # shutil.copytree(r"C:\genesis\fw\jobs\{}".format(job_name_1),r"C:\cc\jobs\{}".format(job_name_1))
+        # time.sleep(15)
+        if _type == 1:
+            # with open(features,"r") as f:
+            #     s=f.readlines()
+            #     print(s[3])
+            # if "r0" in s[3]:
+            #     print("比对发现有差异！！！！！！")
+            #     result = "错误"
+            # else:
+            #     print("恭喜！比对通过！！！")
+            #     result = "正常"
+
+            diff = False
+            asw_dir = os.path.dirname(self.asw_env)
+            matrix_path = os.path.join(asw_dir, 'fw/jobs/{}/matrix/matrix'.format(job1))
+            with open(matrix_path, 'r') as f:
+                for var in f.readlines():
+                    line = var.strip()
+                    if len(line) == 0:
+                        continue
+                    attr = line.split('=')
+                    if len(attr) == 2:
+                        # print(attr)
+                        if attr[0] == 'NAME' and attr[1].lower() == layer_cp:
+                            diff = True
+                            break
+            if diff == True:
+                print('Difference were found')
+                result = "错误"
+            else:
+                print('Layers Match')
+                result = "正常"
+            for cmd in cmd_list2:
+                self.exec_cmd(cmd)
+        return result
 
     def import_odb_folder(self, jobpath,*args,**kwargs):
         Print().print_with_delimiter('import job')
         results=[]
         self.jobpath = jobpath
+
         if "job_name" in kwargs:
+            # print("job_name of para:",kwargs['job_name'])
             job_name = kwargs['job_name']
         else:
             job_name = os.path.basename(self.jobpath)
-        cmd_list = [
+        # print("job_name now is:",job_name)
+        cmd_list1 = [
             'COM import_job,db=genesis,path={},name={},analyze_surfaces=no'.format(jobpath, job_name.lower()),
+
         ]
-        for cmd in cmd_list:
+
+        for cmd in cmd_list1:
             print(cmd)
             ret = self.exec_cmd(cmd)
             results=results.append(ret)
+            if ret != 0:
+                print('inner error')
+                return results
+
         return results
 
     def layer_compare_g_open_2_job(self, *args,**kwargs):
@@ -561,7 +724,134 @@ class Asw():
                 return False
         return True
 
+    def Gerber2ODB2(self, paras, _type,job_id):
+        # print("*"*100,"gerber2odb")
 
+
+
+        try:
+            path = paras['path']
+            job = paras['job']
+            step = paras['step']
+            format = paras['format']
+            data_type = paras['data_type']
+            units = paras['units']
+            coordinates = paras['coordinates']
+            zeroes = paras['zeroes']
+            nf1 = paras['nf1']
+            nf2 = paras['nf2']
+            decimal = paras['decimal']
+            separator = paras['separator']
+            tool_units = paras['tool_units']
+            layer = paras['layer']
+            print("layer"*10,layer)
+            layer=layer.replace(' ','-').replace('(', '-').replace(')', '-')
+            print("layer" * 10, layer)
+            wheel = paras['wheel']
+            wheel_template = paras['wheel_template']
+            nf_comp = paras['nf_comp']
+            multiplier = paras['multiplier']
+            text_line_width = paras['text_line_width']
+            signed_coords = paras['signed_coords']
+            break_sr = paras['break_sr']
+            drill_only = paras['drill_only']
+            merge_by_rule = paras['merge_by_rule']
+            threshold = paras['threshold']
+            resolution = paras['resolution']
+        except Exception as e:
+            print(e)
+            return False
+
+        try:
+            print("开始定位"*10)
+            job_current = models.Job.objects.get(id=job_id)
+            layer_all = models.Layer.objects.filter(job=job_current)
+            print(layer_all)
+            print(path.replace(' ', '-').replace('(', '-').replace(')', '-'))
+            print(os.path.basename(path).replace(' ', '-').replace('(', '-').replace(')', '-'))
+            layer_e2 = models.Layer.objects.get(job=job_current, layer=os.path.basename(path).replace(' ', '-').replace('(', '-').replace(')', '-'))
+            print('*'*100,layer_e2)
+            print("*"*100,layer_e2.layer_file_type)
+            if layer_e2.status == 'published' and layer_e2.layer_file_type=='excellon2':
+                pass
+                print("我是Excellon2!!!!!")
+                format='Excellon2'
+                units=layer_e2.units_g.lower()
+                zeroes=layer_e2.zeroes_omitted_g.lower()
+                nf1 = int(layer_e2.number_format_A_g)
+                nf2 = int(layer_e2.number_format_B_g)
+                #g软件的tool_units没有mils选项
+                if layer_e2.tool_units_g.lower() == 'mils':
+                    tool_units = 'inch'
+                else:
+                    tool_units = layer_e2.tool_units_g.lower()
+
+                separator='nl'
+            else:
+                print("我不是孔Excellon2!")
+
+            print("结束定位" * 10)
+        except:
+            pass
+            print("有异常啊！")
+        # print("p"*100,path)
+
+        # if not os.path.exists(path):
+        #     print('{} does not exist'.format(path))
+        #     return False
+
+
+
+
+        trans_COM = 'COM input_manual_set,'
+        trans_COM += 'path={},job={},step={},format={},data_type={},units={},coordinates={},zeroes={},'.format(path.replace("\\","/"),
+                                                                                                               job,
+                                                                                                               step,
+                                                                                                               format,
+                                                                                                               data_type,
+                                                                                                               units,
+                                                                                                               coordinates,
+                                                                                                               zeroes)
+        trans_COM += 'nf1={},nf2={},decimal={},separator={},tool_units={},layer={},wheel={},wheel_template={},'.format(
+            nf1, nf2, decimal, separator, tool_units, layer, wheel, wheel_template)
+        trans_COM += 'nf_comp={},multiplier={},text_line_width={},signed_coords={},break_sr={},drill_only={},'.format(
+            nf_comp, multiplier, text_line_width, signed_coords, break_sr, drill_only)
+        trans_COM += 'merge_by_rule={},threshold={},resolution={}'.format(merge_by_rule, threshold, resolution)
+
+        cmd_list1 = []
+        cmd_list2 = []
+        # trans_COM = 'COM input_manual_set,path=C:/Users/EPSZ15/Desktop/2222/YH-DT3.9-FM1921_64X64-8SF2-04.GTL,job=6566,step=777,format=Gerber274x,data_type=ascii,units=mm,coordinates=absolute,zeroes=leading,nf1=4,nf2=4,decimal=no,separator=*,tool_units=inch,layer=yh-dt3.9-fm1921_64x64-8sf2-04.gtl,wheel=,wheel_template=,nf_comp=0,multiplier=1,text_line_width=0.0024,signed_coords=no,break_sr=yes,drill_only=no,merge_by_rule=no,threshold=200,resolution=3'
+        if _type == 0:
+            cmd_list1 = [
+                'COM input_manual_reset',
+                # 'COM input_manual_set,path={},job={},step={},format={},data_type{},units={},coordinates={},zeroes={},nf1={},nf2={},decimal={},separator={},\
+                #     tool_units={},layer={},wheel={},wheel_template={},nf_comp={},multiplier={},text_line_width={},signed_coords={},break_sr={},drill_only={},\
+                #     merge_by_rule={},threshold={},resolution={}'.format(path, job, step, format, data_type, units, coordinates, zeroes, nf1, nf2, decimal,
+                #     separator, tool_units, layer, wheel, wheel_template, nf_comp, multiplier, text_line_width, signed_coords, break_sr, drill_only, merge_by_rule,
+                #     threshold, resolution),
+                trans_COM,
+                ('COM input_manual,script_path={}'.format(''))
+            ]
+            cmd_list2 = [
+                'COM input_manual_reset',
+                trans_COM,
+                'COM input_manual,script_path={}'.format('')
+            ]
+        else:
+            cmd_list1 = [
+                'COM save_job,job={},override=no'.format(job)
+            ]
+            cmd_list2 = [
+                'COM save_job,job={},override=no'.format(job)
+            ]
+
+        for cmd in cmd_list1:
+            print(cmd)
+            ret = self.exec_cmd(cmd)
+            if ret != 0:
+                print('inner error')
+                return False
+        return True
 
     def Gerber2ODB2_no_django(self, paras, _type,job_id,*args,**kwargs):
         # print("*"*100,"gerber2odb")
