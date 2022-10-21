f5f94f9ab5c1b8e306ca473f6f998156bf85d179
naivete13sky <chen320821@163.com>
2022-10-21 10:15:25 +0800

    20221021 5 ok

--- gl.py
+++ gl.py
@@ -1,6 +1,4 @@
 all_layer=None
-
-
 username="admin"
 password=""
 all_files=""
@@ -8,5 +6,4 @@ file_job = 'temp'
 step = 'orig'
 pass_layers = []
 rets=None
-# g_getway_path=r"C:\EPSemicon\cc\gateway.exe"
-g_getway_path=r"C:\cc\python\epwork\epcam_test_client\config_g\bin\gateway.exe"
+
